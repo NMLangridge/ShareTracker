d61ce0519391b24002093bde338f243bd1cde2b3
Nathan Langridge <nathanlangridge@hotmail.com>
2019-10-18 10:20:20 +0100

    added mounted to App.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,28 +1,19 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="html">
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
-  components: {
-    HelloWorld
-  }
+
+  mounted(){
+    fetch("http://localhost:3000/")
+    .then(response => response.json())
+    .then(json => console.log(json));
+  },
+
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="css" scoped>
 </style>

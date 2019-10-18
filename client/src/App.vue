b555d4c58be169300ec074ee8e0ee006cd955436
Nathan Langridge <nathanlangridge@hotmail.com>
2019-10-18 10:55:11 +0100

    apiKey hidden

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,15 +3,20 @@
 </template>
 
 <script>
+import apiKey from './assets/secretConfig.js';
+
 export default {
   name: 'app',
-
-  mounted(){
-    fetch("http://localhost:3000/")
-    .then(response => response.json())
-    .then(json => console.log(json));
+  data() {
+    return {
+      shareData: []
+    }
   },
-
+  mounted(){
+    fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=MSFT&apikey=${apiKey}`)
+    .then(response => response.json())
+    .then(shareData => this.shareData = shareData)
+  }
 }
 </script>
 

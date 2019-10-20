03b09c303f280606cb460cd941e5e45c1f931e2e
EDownie <53910027+ED1978@users.noreply.github.com>
2019-10-20 10:58:28 +0100

    fetched fb data

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,5 +1,9 @@
 <template lang="html">
-
+  <div class="app-wrapper">
+    <div id="Header bar">
+        <h1>Share Tracker</h1>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -9,13 +13,24 @@ export default {
   name: 'app',
   data() {
     return {
-      shareData: []
+      msftData: null,
+      fbData: null,
+      applData: null,
+      amznData: null,
+      nflxData: null,
+      tslaData: null,
+      sbuxData: null
     }
   },
   mounted(){
     fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=MSFT&apikey=${apiKey}`)
-    .then(response => response.json())
-    .then(shareData => this.shareData = shareData)
+    .then(res => res.json())
+    .then(msftData => this.msftData = msftData);
+
+    fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=FB&apikey=${apiKey}`)
+    .then(res => res.json())
+    .then(fbData => this.fbData = fbData);
+
   }
 }
 </script>

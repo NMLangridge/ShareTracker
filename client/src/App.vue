91e18eb7ae19ee906787cf15b1f32b618715f234
EDownie <53910027+ED1978@users.noreply.github.com>
2019-10-20 12:10:29 +0100

    fetches changed to get daily timne series

--- client/src/App.vue
+++ client/src/App.vue
@@ -24,27 +24,27 @@ export default {
     }
   },
   mounted(){
-    fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=MSFT&apikey=${apiKeyA}`)
+    fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=${apiKeyA}`)
     .then(res => res.json())
     .then(msftData => this.msftData = msftData);
 
-    fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=FB&apikey=${apiKeyA}`)
+    fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=FB&apikey=${apiKeyA}`)
     .then(res => res.json())
     .then(fbData => this.fbData = fbData);
 
-    fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=AAPL&apikey=${apiKeyB}`)
+    fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=AAPL&apikey=${apiKeyB}`)
     .then(res => res.json())
     .then(aaplData => this.aaplData = aaplData);
 
-    fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=AMZN&apikey=${apiKeyB}`)
+    fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=AMZN&apikey=${apiKeyB}`)
     .then(res => res.json())
     .then(amznData => this.amznData = amznData);
 
-    fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=NFLX&apikey=${apiKeyC}`)
+    fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=NFLX&apikey=${apiKeyC}`)
     .then(res => res.json())
     .then(nflxData => this.nflxData =nflxData);
 
-    fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=TSLA&apikey=${apiKeyC}`)
+    fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=TSLA&apikey=${apiKeyC}`)
     .then(res => res.json())
     .then(tslaData => this.tslaData = tslaData);
   }

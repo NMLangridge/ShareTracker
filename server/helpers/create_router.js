26318bd06cd0b016cfa3bea3fcfb934f8fe76ab0
Nathan Langridge <nathanlangridge@hotmail.com>
2019-10-20 10:47:05 +0100

    added helpers folder and create_router.js

---
+++ server/helpers/create_router.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const ObjectID = require('mongodb').ObjectID;
+
+const createRouter = function(collection) {
+
+  const router = express.Router();
+
+  return router;
+
+}
+
+module.exports = createRouter;
--- server/server.js
+++ server/server.js
@@ -2,10 +2,19 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 app.use(cors());
+const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient;
+const createRouter = require('./helpers/create_router.js');
+app.use(bodyParser.json());
 
-app.get('/', function(req, res) {
-  res.send('Hello Eric!');
-});
+MongoClient.connect('mongodb://localhost:27017')
+.then((client) => {
+  const db = client.db('shareData');
+  const boughtPriceCollection = db.collection('boughtPrice');
+  const boughtPriceRouter = createRouter(boughtPriceCollection);
+  app.use('/api/boughtPrice', boughtPriceRouter);
+})
+.catch(console.err);
 
 app.listen(3000, function() {
   console.log('App running on port 3000');

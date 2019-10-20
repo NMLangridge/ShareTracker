4510e19ad972fee54c85ba675cdefa9efdde3dad
EDownie <53910027+ED1978@users.noreply.github.com>
2019-10-20 11:27:37 +0100

    all companies fetched and set with multiple keys

--- client/src/App.vue
+++ client/src/App.vue
@@ -7,7 +7,9 @@
 </template>
 
 <script>
-import apiKey from './assets/secretConfig.js';
+import apiKeyA from './assets/secretConfig.js';
+import apiKeyB from './assets/secretConfig.js';
+import apiKeyC from './assets/secretConfig.js';
 
 export default {
   name: 'app',
@@ -18,23 +20,33 @@ export default {
       aaplData: null,
       amznData: null,
       nflxData: null,
-      tslaData: null,
-      sbuxData: null
+      tslaData: null
     }
   },
   mounted(){
-    fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=MSFT&apikey=${apiKey}`)
+    fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=MSFT&apikey=${apiKeyA}`)
     .then(res => res.json())
     .then(msftData => this.msftData = msftData);
 
-    fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=FB&apikey=${apiKey}`)
+    fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=FB&apikey=${apiKeyA}`)
     .then(res => res.json())
     .then(fbData => this.fbData = fbData);
 
-    fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=AAPL&apikey=${apiKey}`)
+    fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=AAPL&apikey=${apiKeyB}`)
     .then(res => res.json())
     .then(aaplData => this.aaplData = aaplData);
 
+    fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=AMZN&apikey=${apiKeyB}`)
+    .then(res => res.json())
+    .then(amznData => this.amznData = amznData);
+
+    fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=NFLX&apikey=${apiKeyC}`)
+    .then(res => res.json())
+    .then(nflxData => this.nflxData =nflxData);
+
+    fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=TSLA&apikey=${apiKeyC}`)
+    .then(res => res.json())
+    .then(tslaData => this.tslaData = tslaData);
   }
 }
 </script>

8b5071a947887441a166bdbe982b087d535045da
Nathan Langridge <nathanlangridge@hotmail.com>
2019-10-20 14:22:22 +0100

    added user stock selector, created dropdown

--- client/src/App.vue
+++ client/src/App.vue
@@ -4,6 +4,7 @@
         <h1>Share Tracker</h1>
     </div>
     <user-profile :shares="currentShares"/>
+    <user-stock-selector :MSFT="msftData" :FB="fbData" :AAPL="aaplData" :AMZN="amznData" :NFLX="nflxData" :TSLA="tslaData"/>
   </div>
 </template>
 
@@ -13,6 +14,7 @@ import apiKeyB from './assets/secretConfig.js';
 import apiKeyC from './assets/secretConfig.js';
 import ShareService from './services/ShareService.js';
 import UserProfile from './components/UserProfile.vue';
+import UserStockSelector from './components/UserStockSelector.vue';
 
 export default {
   name: 'app',
@@ -28,29 +30,29 @@ export default {
     }
   },
   mounted(){
-    // fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=${apiKeyA}`)
-    // .then(res => res.json())
-    // .then(msftData => this.msftData = msftData);
-    //
-    // fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=FB&apikey=${apiKeyA}`)
-    // .then(res => res.json())
-    // .then(fbData => this.fbData = fbData);
-    //
-    // fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=AAPL&apikey=${apiKeyB}`)
-    // .then(res => res.json())
-    // .then(aaplData => this.aaplData = aaplData);
-    //
-    // fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=AMZN&apikey=${apiKeyB}`)
-    // .then(res => res.json())
-    // .then(amznData => this.amznData = amznData);
-    //
-    // fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=NFLX&apikey=${apiKeyC}`)
-    // .then(res => res.json())
-    // .then(nflxData => this.nflxData =nflxData);
-    //
-    // fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=TSLA&apikey=${apiKeyC}`)
-    // .then(res => res.json())
-    // .then(tslaData => this.tslaData = tslaData);
+    fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=${apiKeyA}`)
+    .then(res => res.json())
+    .then(msftData => this.msftData = msftData);
+
+    fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=FB&apikey=${apiKeyA}`)
+    .then(res => res.json())
+    .then(fbData => this.fbData = fbData);
+
+    fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=AAPL&apikey=${apiKeyB}`)
+    .then(res => res.json())
+    .then(aaplData => this.aaplData = aaplData);
+
+    fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=AMZN&apikey=${apiKeyB}`)
+    .then(res => res.json())
+    .then(amznData => this.amznData = amznData);
+
+    fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=NFLX&apikey=${apiKeyC}`)
+    .then(res => res.json())
+    .then(nflxData => this.nflxData =nflxData);
+
+    fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=TSLA&apikey=${apiKeyC}`)
+    .then(res => res.json())
+    .then(tslaData => this.tslaData = tslaData);
 
     this.fetchData();
   },
@@ -62,7 +64,8 @@ export default {
     }
   },
   components: {
-    'user-profile': UserProfile
+    'user-profile': UserProfile,
+    'user-stock-selector': UserStockSelector
   }
 }
 </script>
---
+++ client/src/components/UserStockSelector.vue
@@ -0,0 +1,28 @@
+<template lang="html">
+  <div>
+    <select v-model="stock">
+      <option disabled value="">Select Company</option>
+      <option value="MSFT">Microsoft</option>
+      <option value="FB">Facebook</option>
+      <option value="AAPL">Apple</option>
+      <option value="AMZN">Amazon</option>
+      <option value="NFLX">Netflix</option>
+      <option value="TSLA">Tesla</option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'stock-selector',
+  data(){
+    return {
+      stock: null
+    }
+  },
+  props: ['MSFT', 'FB', 'AAPL', 'AMZN', 'NFLX', 'TSLA']
+}
+</script>
+
+<style lang="css" scoped>
+</style>

668895da8edf7efc2e547c229f394be9f8ebf0fe
EDownie <53910027+ED1978@users.noreply.github.com>
2019-10-20 11:50:59 +0100

    user profile creted

---
+++ client/src/components/UserProfile.vue
@@ -0,0 +1,17 @@
+<template lang="html">
+
+</template>
+
+<script>
+export default {
+  name: 'user-profile',
+  data() {
+    return{
+      name: 'Nathan Buffet'
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>

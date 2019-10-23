ee713d4720c854f0779c2dc1359727d7dfe1935a
EDownie <53910027+ED1978@users.noreply.github.com>
2019-10-23 11:21:20 +0100

    api reactivated

--- client/src/App.vue
+++ client/src/App.vue
@@ -35,29 +35,29 @@ export default {
     }
   },
   mounted(){
-    fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=demo`)
+    fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=${apiKeyA}`)
     .then(res => res.json())
     .then(msftData => this.msftData = msftData);
 
-    // fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=FB&apikey=${apiKeyA}`)
-    // .then(res => res.json())
-    // .then(fbData => this.fbData = fbData);
-    //
-    // fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=AAPL&apikey=${apiKeyB}`)
-    // .then(res => res.json())
-    // .then(aaplData => this.aaplData = aaplData);
-    //
-    // fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=AMZN&apikey=${apiKeyB}`)
-    // .then(res => res.json())
-    // .then(amznData => this.amznData = amznData);
-    //
-    // fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=SBUX&apikey=${apiKeyC}`)
-    // .then(res => res.json())
-    // .then(sbuxData => this.sbuxData = sbuxData);
-    //
-    // fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=TSLA&apikey=${apiKeyC}`)
-    // .then(res => res.json())
-    // .then(tslaData => this.tslaData = tslaData);
+    fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=FB&apikey=${apiKeyA}`)
+    .then(res => res.json())
+    .then(fbData => this.fbData = fbData);
+
+    fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=AAPL&apikey=${apiKeyB}`)
+    .then(res => res.json())
+    .then(aaplData => this.aaplData = aaplData);
+
+    fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=AMZN&apikey=${apiKeyB}`)
+    .then(res => res.json())
+    .then(amznData => this.amznData = amznData);
+
+    fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=SBUX&apikey=${apiKeyC}`)
+    .then(res => res.json())
+    .then(sbuxData => this.sbuxData = sbuxData);
+
+    fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=TSLA&apikey=${apiKeyC}`)
+    .then(res => res.json())
+    .then(tslaData => this.tslaData = tslaData);
 
     this.fetchData();
 
